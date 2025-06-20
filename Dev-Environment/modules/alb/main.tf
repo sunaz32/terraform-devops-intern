c3d7dfdb0978c47d88f25f99a73c8e82d80eeff1
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-06-20 13:39:53 +0530

    alb/main.tf updated

--- Dev-Environment/modules/alb/main.tf
+++ Dev-Environment/modules/alb/main.tf
@@ -56,18 +56,3 @@ output "alb_dns" {
   description = "ALB DNS Name"
   value       = aws_lb.app.dns_name
 }
-
-output "target_group_arn" {
-  description = "Target group ARN"
-  value       = aws_lb_target_group.app.arn
-}
-
-output "alb_sg_id" {
-  description = "ALB security group ID"
-  value       = aws_security_group.alb_sg.id
-}
-
-output "alb_zone_id" {
-  description = "ALB zone ID (used for Route 53 if needed)"
-  value       = aws_lb.app.zone_id
-}
--- Dev-Environment/modules/alb/outputs.tf
+++ Dev-Environment/modules/alb/outputs.tf
@@ -1,3 +1,18 @@
 output "alb_sg_id" {
   value = aws_security_group.alb_sg.id  # Make sure this matches your SG resource name
 }
+
+output "alb_dns" {
+  description = "ALB DNS Name"
+  value       = aws_lb.app.dns_name
+}
+
+output "target_group_arn" {
+  description = "Target group ARN"
+  value       = aws_lb_target_group.app.arn
+}
+
+output "alb_zone_id" {
+  description = "ALB zone ID (used for Route 53 if needed)"
+  value       = aws_lb.app.zone_id
+}
