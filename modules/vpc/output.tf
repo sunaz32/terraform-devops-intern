ab41893769253c8b7c727636c46c82a098ddefeb
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-06-27 02:40:13 +0530

    network mode change

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -3,9 +3,11 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-  value = [for s in aws_subnet.public : s.id]
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = [for subnet in aws_subnet.public : subnet.id]
 }
+
 
 output "private_subnet_ids" {
   value = [for s in aws_subnet.private : s.id]
