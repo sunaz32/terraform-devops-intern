c207363fd2bcef1c78ea71dd56a1693424fd93f5
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-06-27 00:51:28 +0530

    iam role adfded

--- modules/ecs-ec2/main.tf
+++ modules/ecs-ec2/main.tf
@@ -15,12 +15,44 @@ resource "local_file" "private_key" {
   directory_permission = "0700"
 }
 
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${var.app_name}-ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_attach" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${var.app_name}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+
 
 resource "aws_launch_template" "ecs" {
   name_prefix   = "${var.app_name}-ecs-launch-"
   image_id = var.ami_id
   instance_type = var.ecs_instance_type
   key_name      = aws_key_pair.ecs_key.key_name
+iam_instance_profile {
+  name = aws_iam_instance_profile.ecs_instance_profile.name
+}
+
 
   user_data = base64encode(<<EOF
 #!/bin/bash
