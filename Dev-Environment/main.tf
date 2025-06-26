17c932b90925421adc14d4d42624964c03184097
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-06-26 12:16:56 +0530

    domain updated

--- Dev-Environment/main.tf
+++ Dev-Environment/main.tf
@@ -23,8 +23,6 @@ module "alb" {
   app_name          = "naz-dev-app"
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
-  alb_zone_name  = var.alb_zone_name
-   alb_domain       = var.alb_domain
    alb_sg_id         =[ module.security_group.alb_sg_id]
     environment         = var.environment
 }
