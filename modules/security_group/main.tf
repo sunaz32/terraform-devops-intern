317aace5ec49b1181f66dc00cb57850dde48a913
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-06-27 01:54:06 +0530

    security group updated

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   =5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -27,6 +27,19 @@ resource "aws_security_group" "ecs_sg" {
     to_port         = 5000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
