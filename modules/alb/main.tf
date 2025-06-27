e0e545995aa1400a33b7322e0fd9bffd281fb86f
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-06-27 12:45:51 +0530

    network mode change

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -36,7 +36,7 @@ resource "aws_lb_target_group" "app_tg" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
-  port              = 80
+  port              = 5000
   protocol          = "HTTP"
 
   default_action {
