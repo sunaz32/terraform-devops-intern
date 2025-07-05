75a6646ffa6b7b6bbc0ef1571498392963592fb9
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-07-05 15:14:32 +0530

    all files deleted

--- modules/bastion/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bastion_public_ip" {
-  description = "Public IP of the bastion host"
-  value       = aws_instance.bastion.public_ip
-}
--- modules/security_group/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "vpc_id" {}
-variable "env" {}
-variable "allowed_ssh_cidr" {
-  description = "CIDR block allowed to SSH into bastion (e.g. your IP)"
-  type        = string
-}
