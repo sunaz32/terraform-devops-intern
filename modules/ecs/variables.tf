aa1f9d30d2fc9e51f480f0fb0b5fd3f81b24983f
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-07-06 02:06:33 +0530

    main.tf updated

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,8 +1,6 @@
 variable "app_name" {}
 variable "image_url" {}
-variable "public_subnet_ids" {
-  type = list(string)
-}
+
 variable "alb_target_group_arn" {
   description = "ARN of the ALB target group"
   type        = string
