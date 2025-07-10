6c623f294d7b915dedf4228b9a2a8a63e78e7c69
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-07-10 17:05:17 +0530

    backend file updated

--- dev-environment/backend.tf
+++ dev-environment/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-bucket-dev-naziya"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-dev-naziya"
+    dynamodb_table = "naz-dev-terraform-locks"
     encrypt        = true
   }
 }
