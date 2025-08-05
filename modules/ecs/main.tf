8e5c5cc570104f900329869365d2f8242edbbc23
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-08-05 16:27:19 +0530

    image url updated

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -12,7 +12,7 @@ resource "aws_ecs_task_definition" "this" {
 
   container_definitions = jsonencode([{
     name      = "app"
-    image     = var.image_url
+    image     =  var.image_url != "" ? var.image_url : "851725602228.dkr.ecr.ap-south-1.amazonaws.com/naz-stage-intern-app:latest"
     essential = true
     portMappings = [{
       containerPort = 5000
