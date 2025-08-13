26c28cf82637edf65d34c095e70f1ccd6f75438c
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-08-13 11:34:36 +0530

    file formated

--- dev-environment/main.tf
+++ dev-environment/main.tf
@@ -19,12 +19,12 @@ module "security_group" {
 }
 
 module "alb" {
-  source              = "../modules/alb"
-  app_name            = var.app_name
-  vpc_id              = module.vpc.vpc_id
-  alb_sg_id           = module.security_group.alb_sg_id
-  public_subnet_ids   = module.vpc.public_subnet_ids
-  alb_domain          = var.alb_domain
+  source            = "../modules/alb"
+  app_name          = var.app_name
+  vpc_id            = module.vpc.vpc_id
+  alb_sg_id         = module.security_group.alb_sg_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  alb_domain        = var.alb_domain
 }
 
 
