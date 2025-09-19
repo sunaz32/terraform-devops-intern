98a7f005993665be7726510c7c03de3a94800e26
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-09-19 13:51:27 +0530

    listner change

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -22,20 +22,14 @@ resource "aws_lb_target_group" "this" {
   }
 }
 
-# Redirect HTTP to HTTPS
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
   }
 }
 
