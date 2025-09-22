fe8eff2971a137a4217cf739d8514e33b1b15727
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-09-22 18:32:31 +0530

    backend change

--- stage-environment/backend.tf
+++ stage-environment/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-stage-naziya"
+    bucket         = "naz-terraform-state-stage"
     key            = "stage/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "naz-stage-terraform-locks"
+    dynamodb_table = "stage-lock-table"
     encrypt        = true
   }
 }
