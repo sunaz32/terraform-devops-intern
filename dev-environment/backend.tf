aca12a67dac607e7f343fc00fe65d72177967818
sunaz32 <naziya.m@cloudshastrainfotech.com>
2025-10-09 16:31:00 +0530

    new acct added

--- dev-environment/backend.tf
+++ dev-environment/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "naz-terraform-dev1"
+    bucket         = "naz-terraform-dev2"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "naz-dev-lock-table"
